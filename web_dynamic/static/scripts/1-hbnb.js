5b7e54817d5bd78e6c87aae018616c34c7e23668
FlintStoneObed <141341733+FlintStoneObed@users.noreply.github.com>
2024-06-03 16:54:32 +0000

    Update 1-hbnb.js

--- web_dynamic/static/scripts/1-hbnb.js
+++ web_dynamic/static/scripts/1-hbnb.js
@@ -1,14 +1,35 @@
-$(document).ready(init);
+'use strict';
+$(() => {
+  let amenitiesSelected = [];
+  const selectors = {
+    amenitiesHeader: '.amenities > h4',
+    amenityBox: '.amenities > .popover > ul > li > input[type="checkbox"]',
+    amenityItem: '.amenities > .popover > ul > li'
+  };
 
-function init () {
-  const amenityObj = {};
-  $('.amenities .popover input').change(function () {
-    if ($(this).is(':checked')) {
-      amenityObj[$(this).attr('data-name')] = $(this).attr('data-id');
-    } else if ($(this).is(':not(:checked)')) {
-      delete amenityObj[$(this).attr('data-name')];
-    }
-    const names = Object.keys(amenityObj);
-    $('.amenities h4').text(names.sort().join(', '));
+  $(selectors.amenityItem).on('mousedown', ev => {
+    ev.target.getElementsByTagName('input')?.item(0)?.click();
   });
-}
+
+  $(selectors.amenityBox).change(ev => {
+    const amenityId = ev.target.getAttribute('data-id');
+    const amenityName = ev.target.getAttribute('data-name');
+
+    if (ev.target.checked) {
+      if (!amenitiesSelected.find(obj => obj.id === amenityId)) {
+        amenitiesSelected.push({
+          id: amenityId,
+          name: amenityName
+        });
+      }
+    } else {
+      amenitiesSelected = amenitiesSelected.filter(
+        obj => (obj.id !== amenityId) && (obj.name !== amenityName)
+      );
+    }
+    const htmlContent = amenitiesSelected.map(obj => obj.name).join(', ');
+    $(selectors.amenitiesHeader).html(
+      amenitiesSelected.length > 0 ? htmlContent : '&nbsp;'
+    );
+  });
+});
